8517297e3b8aaf383ce6cc0233bbd41e7bc87625
Pablo <pabarragan94@gmail.com>
2021-03-11 19:21:49 +0100

    add version tag

--- auto-perk.user.js
+++ auto-perk.user.js
@@ -6,6 +6,7 @@
 // @description Auto perk
 // @grant       GM_getValue
 // @grant       GM_setValue
+// @version      0.0.1
 // @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @downloadURL https://github.com/pbl0/auto-perk/raw/main/auto-perk.user.js
 // ==/UserScript==
